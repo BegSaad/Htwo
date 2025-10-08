13697563d0554f4266e6401473ad98113fa5ccdf
mohammadsaadbeg <mohdsaadstp2019@gmail.com>
2025-10-08 13:53:41 +0530

    created apk

--- index.js
+++ index.js
@@ -5,5 +5,20 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import * as Sentry from '@sentry/react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+
+// Initialize Sentry
+Sentry.init({
+
+ dsn: 'https://ac6c7a90230ae611552998cc0364f258@o4510146953412608.ingest.de.sentry.io/4510148130898000',
+  tracesSampleRate: 1.0,
+  enableNative: true,
+  debug: true,  // for dev
+});
+ 
+
+const SentryApp = Sentry.wrap(App);
+
+// ✅ Register the wrapped app (not the plain App)
+AppRegistry.registerComponent(appName, () => SentryApp);
---
+++ src/Components/CTButton.tsx
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+const CTButton=()=>{
+
+    return(
+        <View>
+            <Text>  hsh</Text>
+        </View>
+    )
+}
+export default CTButton
+
+const styles = StyleSheet.create({})    
